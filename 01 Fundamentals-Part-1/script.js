75b2a273eae711ca0c758f5bc0b8b7f8112818f5
Mehul Saxena <mehulsaxena45@gmail.com>
2022-12-02 18:00:16 +0530

    node install for js

--- 01 Fundamentals-Part-1/script.js
+++ 01 Fundamentals-Part-1/script.js
@@ -78,3 +78,53 @@ let jBMI = BMICAL(jm , jh);
 let markHigherBMI = mBMI > jBMI ;
 
 console.log(mBMI , " " , jBMI , " " , markHigherBMI);
+
+// Template litereals
+
+const name = "Mehul Saxena";
+const senetence = `My Name is ${name}`;
+console.log(senetence);
+
+// replacement of \n in the code using the template literals
+console.log(`String
+multiple 
+lines.
+`);
+
+
+//  Decision making
+const age = 19;
+const isOldEnough = age >= 18;
+
+if(isOldEnough){
+    console.log('Sarah can start driving licence .');
+}
+else{
+    const yearLeft = 18 -age
+    console.log(`sarah is too young . Wait another ${yearLeft} years.`);
+}
+
+const birthYear = 1991;
+let century;
+if(birthYear <= 2000){
+    century = 20;
+}
+else{
+    century = 21;
+
+}
+
+console.log(century);
+
+/////////////////////////////////////////////////////////////////////////
+// Coding Change #2
+
+/* 
+    Use the BMI example from chanllenge 1, and the code you already wrote , and improve it.
+    1. Print a nice output to the console, saying who has the higher BMI, The message 
+    can be either  "Marks BMI is higher than Johns" or "Johns BMI is higer than Marks".
+
+    2. Use a template string to include 
+    */
+
+
---
+++ JS_Interview_Questions/index.js
@@ -0,0 +1,16 @@
+function real(){
+    console.log("I am the real Always run me ");
+}
+
+
+function real(){
+    console.log(" No im the real one ");
+}
+
+real();
+
+function real(){
+    console.log("You both are wasted");
+}
+
+
